57f84b9eee453cd76dd9803f8dfb382e65a9b25a
mahak161 <mahak.gla_bca.ds25@gla.ac.in>
2026-07-04 08:53:04 -0700

    Update Analytics.py

--- Customer-Churn-System/pages/Analytics.py
+++ Customer-Churn-System/pages/Analytics.py
@@ -18,7 +18,14 @@ st.set_page_config(
 # -----------------------------
 # LOAD CSS
 # -----------------------------
-with open("assets/styles.css") as f:
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+css_path = os.path.join(BASE_DIR, "assets", "styles.css")
+
+with open(css_path) as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
     st.markdown(
         f"<style>{f.read()}</style>",
         unsafe_allow_html=True
