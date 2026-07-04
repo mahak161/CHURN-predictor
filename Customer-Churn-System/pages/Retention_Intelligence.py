ca798e273a57cf6ce1353cb7ad43e658089ee78a
mahak161 <mahak.gla_bca.ds25@gla.ac.in>
2026-07-04 09:05:58 -0700

    Update Retention_Intelligence.py

--- Customer-Churn-System/pages/Retention_Intelligence.py
+++ Customer-Churn-System/pages/Retention_Intelligence.py
@@ -115,7 +115,14 @@ else:
         "😊 Continue regular engagement."
     )
 
-with open("assets/styles.css") as f:
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+css_path = os.path.join(BASE_DIR, "assets", "styles.css")
+
+with open(css_path) as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
     st.markdown(
         f"<style>{f.read()}</style>",
         unsafe_allow_html=True
